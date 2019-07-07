c411093f2ef50b7c81c1af0228387840e31cdcaf
mirgee <mrslvkvr@gmail.com>
2019-07-07 14:57:44 +0200

    Get basic template working

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:////home/miroslav/Source/blog/blog.db'
+
+db = SQLAlchemy(app)
+
+class Blogbpost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    subtitle = db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    date_posted = db.Column(db.DateTime)
+    content = db.Column(db.Text)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/post')
+def post():
+    return render_template('post.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
