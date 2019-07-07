c0b26d5a24badf01671673f906450b7ae9540ab5
mirgee <mrslvkvr@gmail.com>
2019-07-07 18:02:03 +0200

    Database, adding posts, showing posts working

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -8,7 +9,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
 
 db = SQLAlchemy(app)
 
-class Blogbpost(db.Model):
+
+class Blogpost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     subtitle = db.Column(db.String(50))
@@ -19,7 +21,9 @@ class Blogbpost(db.Model):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = Blogpost.query.order_by(Blogpost.date_posted.desc()).all()
+
+    return render_template('index.html', posts=posts)
 
 
 @app.route('/about')
@@ -27,14 +31,38 @@ def about():
     return render_template('about.html')
 
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = Blogpost.query.filter_by(id=post_id).one()
+
+    return render_template('post.html', post=post)
 
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    print(request.form)
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = Blogpost(title=title, subtitle=subtitle, author=author,
+                     content=content, date_posted=datetime.now())
+
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
 
 
 if __name__ == '__main__':
